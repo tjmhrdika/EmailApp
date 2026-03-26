1e02dd56d4bdce37732e849646cd6bbf276f7752
fachri <nurfachrialisnan@gmail.com>
2026-03-26 16:34:40 +0700

    fix authentication structure folder

--- Services/CustomAuthenticationStateProvider.cs
+++ Services/CustomAuthenticationStateProvider.cs
@@ -1,4 +1,4 @@
-using System.Security.Claims;
+    using System.Security.Claims;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.JSInterop;
