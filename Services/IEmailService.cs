964a596763d1c610085c0073ca60666dd0278927
fachri <nurfachrialisnan@gmail.com>
2026-06-09 16:32:32 +0700

    fix: remove all trash

--- Services/IEmailService.cs
+++
@@ -1,8 +0,0 @@
-namespace EmailApp.Services
-{
-    public interface IEmailService
-    {
-        Task SendAsync(string to, string subject, string body);
-        Task SendBulkEmailAsync(IEnumerable<string> recipients, string subject, string body);
-    }
-}
